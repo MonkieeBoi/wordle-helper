67d9d112b8a7225b4702b927e1b9949a54cd0f86
MonkieeBoi <danny.pham26425@gmail.com>
2025-08-17 17:55:00 +1000

    feat: active word

--- cmd/wordle-helper/main.go
+++ cmd/wordle-helper/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/MonkieeBoi/wordle-helper/internal/filter"
 	"github.com/MonkieeBoi/wordle-helper/internal/list"
 	"github.com/MonkieeBoi/wordle-helper/internal/wordle"
 	tea "github.com/charmbracelet/bubbletea"
@@ -15,12 +16,12 @@ type styles struct {
 	wordleStyles map[wordle.Colour]lipgloss.Style
 }
 
-var letters map[rune]bool
-
 type model struct {
 	wordle wordle.Wordle
 	list   list.Model
 	styles styles
+	word   wordle.Word
+	active int
 }
 
 func initialModel() model {
@@ -37,13 +38,21 @@ func initialModel() model {
 				Foreground(lipgloss.Color("7")),
 			wordle.EMPTY: lipgloss.NewStyle().
 				Background(lipgloss.Color("0")).
-				Foreground(lipgloss.Color("7")),
+				Foreground(lipgloss.Color("7")).
+				Blink(true),
 		},
 	}
 	m := model{
 		wordle: wordle.NewWordle(),
 		list:   list.New(),
 		styles: s,
+		word: wordle.Word{
+			wordle.Char{Val: '_'},
+			wordle.Char{Val: ' '},
+			wordle.Char{Val: ' '},
+			wordle.Char{Val: ' '},
+			wordle.Char{Val: ' '},
+		},
 	}
 	return m
 }
@@ -61,7 +70,19 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
 		switch msg.String() {
-		case "ctrl+c", "q":
+		case "ctrl+g":
+			m.word[m.active].Colour = wordle.GREEN
+		case "ctrl+y":
+			m.word[m.active].Colour = wordle.YELLOW
+		case "backspace":
+			if m.active < wordle.WORD_LEN {
+				m.word[m.active].Val = ' '
+				m.word[m.active].Colour = wordle.EMPTY
+			}
+			m.active = max(0, m.active-1)
+			m.word[m.active].Colour = wordle.EMPTY
+			m.word[m.active].Val = '_'
+		case "ctrl+c":
 			cmds = append(cmds, tea.Quit)
 		}
 	}
@@ -78,19 +99,17 @@ func (m model) View() string {
 }
 
 func main() {
-	_ = flag.String("f", "", "Text file containing words on seperate lines")
+	wordFile := flag.String("f", "", "Text file containing words on seperate lines")
 	flag.Parse()
+	err := filter.InitWords(*wordFile)
+	if err != nil {
+		fmt.Println("Could not open word file!")
+		os.Exit(1)
+	}
 	m := initialModel()
 	p := tea.NewProgram(m, tea.WithAltScreen())
 	if _, err := p.Run(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
-	}
-}
-
-func init() {
-	letters = make(map[rune]bool, 'z'-'a'+1)
-	for i := 'a'; i <= 'z'; i++ {
-		letters[rune(i)] = true
 	}
 }
